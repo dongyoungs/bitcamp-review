83e26a672febda9eff6274aba9724d5d1517c31f
dongyoungs <kkssckkssc#gmail.com>
2023-07-26 20:43:19 +0900

    8. 함수화 시킬때 파라미터가 너무 많아, 변수들 전부 static 형태로 할당 및 입력받는 부분에서 prompt 메서드 추가

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -4,42 +4,42 @@ package bitcamp.myapp;
 import java.util.Scanner;
 
 public class App {
+  static Scanner scanner = new Scanner(System.in);
+  static int userId = 1;
+  static int length = 0;
+
+  static final int SIZE = 100;
+  static int[] no = new int[SIZE];
+  static String[] name = new String[SIZE];
+  static String[] email = new String[SIZE];
+  static String[] password = new String[SIZE];
+  static char[] gender = new char[SIZE];
+
   public static void main(String[] args) {
 
     // 키보드 스케너 준비
-    Scanner scanner = new Scanner(System.in);
-    int userId = 1;
-    int length = 0;
-
-    final int SIZE = 100;
-    int[] no = new int[SIZE];
-    String[] name = new String[SIZE];
-    String[] email = new String[SIZE];
-    String[] password = new String[SIZE];
-    char[] gender = new char[SIZE];
 
     printTitle();
 
     for (int i = 0; i < SIZE; i++) {
-      inputMember(scanner, i, name, email, password, gender, no, userId++);
+      inputMember();
       length++;
-      if (!promptContinue(scanner)) {
+      if (!promptContinue()) {
         break;
       }
 
     }
-    printMember(length, no, name, email, gender);
+    printMember();
     scanner.close();
   }
 
-  static void inputMember(Scanner scanner,
-      int i, String[] name, String[] email, String[] password, char[] gender, int[] no, int userId) {
+  static void inputMember() {
     System.out.print("번호? ");
-    no[i] = scanner.nextInt();
+    no[length] = scanner.nextInt();
     System.out.print("이름 ?");
-    name[i] = scanner.next();
+    name[length] = scanner.next();
     System.out.print("이메일? ");
-    email[i] = scanner.next();
+    email[length] = scanner.next();
 
     loop: while (true) {
       System.out.println("성별 ");
@@ -50,19 +50,19 @@ public class App {
 
       switch (genderString) {
         case "1":
-          gender[i] = 'M';
+          gender[length] = 'M';
           break loop;
         case "2":
-          gender[i] = 'W';
+          gender[length] = 'W';
           break loop;
         default:
           System.out.println("잘못된 번호입니다");
       }
     }
-    no[i] = userId++;
+    no[length] = userId++;
   }
 
-  static boolean promptContinue(Scanner scanner) {
+  static boolean promptContinue() {
     System.out.print("계속하시겠습니까?(Y/n) ");
     scanner.nextLine();
     String response = scanner.nextLine();
@@ -77,14 +77,18 @@ public class App {
     System.out.println("-----------------------------------");
   }
 
-  static void printMember(int length, int[] no, String[] name, String[] email, char[] gender) {
+  static void printMember() {
     System.out.println("---------------------------------");
-
     System.out.println("번호, 이름, 이메일, 성별");
     System.out.println("---------------------------------");
 
     for (int i = 0; i < length; i++) {
       System.out.printf("%d, %s, %s, %c\n", no[i], name[i], email[i], gender[i]);
     }
+  }
+
+  static String prompt(String title) {
+    System.out.print(title);
+    return scanner.nextLine();
   }
 }
